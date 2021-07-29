44b6c50110cde66d717d672f206ef0517d31962e
Brianna Kwan <kwanbnn11@gmail.com>
2021-07-29 14:09:07 -0400

    Initial Commit

---
+++ Mini Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Project 2
+//
+//  Created by Brianna Kwan on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
